95d44ec4ae5a024bd99aa947dbff8f90f32b33e5
Anh <addao823@gmail.com>
2021-07-28 12:15:37 -0700

    Initilizing socket.io client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,40 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState([]);
+  const socket = io("/");
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessage((prevMess) => {
+        return { ...prevMess, message };
+      });
+    });
+  }, [socket]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></input>
+        <button>Submit</button>
       </header>
+      {message.map((text) => (
+        <div>{text}</div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
