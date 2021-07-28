dc50632e4da6b38dcc719d01f14ba77a54cf4eb8
Anh <addao823@gmail.com>
2021-07-28 10:39:40 -0700

    Initilizing socket.io server side

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
-
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+app.post("/message", (req, res) => {
+  io.emit("message", req.body);
+  red.sent(req.body);
+});
+
+io.on("connection", () => {
+  console.log("a user is connected");
+});
+
+server.listen(PORT, () => {
   console.log("server is running on port " + PORT);
 });
