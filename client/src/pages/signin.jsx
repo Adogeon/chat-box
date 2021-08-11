46ec84013bca40fa6b7ba4117475de2a475ac11d
Anh <addao823@gmail.com>
2021-08-10 20:01:40 -0700

    Adding basic frame for the SignIn page

---
+++ client/src/pages/signin.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <main class="signInMain">
+      <div class="box">
+        <h1>Sign In</h1>
+        <div>
+          <label for="username">Username</label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input name="password" type="password" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default SignIn;
