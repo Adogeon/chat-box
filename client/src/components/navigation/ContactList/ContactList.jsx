a57679d5fc92abac6561706ba41c7f0915c50784
Anh <addao823@gmail.com>
2021-10-18 16:25:22 -0700

    Adding Tab to Sidebar and ContactList

--- client/src/components/navigation/ContactList/ContactList.jsx
+++ client/src/components/navigation/ContactList/ContactList.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ContactList = () => {
+  const userState = useSelector((state) => state.user);
+
   return (
     <div>
-      List of Contact
+      {userState.contact.map((user) => (
+        <div>{user.username}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ContactList
+export default ContactList;
--- client/src/components/navigation/Sidebar/SideBar.jsx
+++ client/src/components/navigation/Sidebar/SideBar.jsx
@@ -3,20 +3,44 @@ import { useSelector } from "react-redux";
 
 import ConversationList from "../ConversationList/ConverSationList";
 import ContactList from "../ContactList/ContactList";
+import Tab from "../Tab/Tab";
 
+import button from "../../../styles/Button/button.module.css";
+import tab from "../../../styles/Button/button.module.css";
 import style from "./SideBar.module.css";
 
 const SideBar = () => {
   const userState = useSelector((state) => state.user);
 
-  const [displayList, setDisplayList] = useState(["conversation"]);
+  const [displayList, setDisplayList] = useState("conversation");
 
   return (
     <div className={style.slidebar}>
       <div className={style.userArea}>
         <div className={style.userName}>{userState.username}</div>
       </div>
-      <div>Toggle button</div>
+      <div className={style.tabArea}>
+        <Tab
+          defaultClass={`${style.tab}`}
+          activeClass={`${style["tab-active"]}`}
+          onClick={() => {
+            setDisplayList("conversation");
+          }}
+          active={displayList === "conversation"}
+        >
+          Messages
+        </Tab>
+        <Tab
+          defaultClass={`${style.tab}`}
+          activeClass={`${style["tab-active"]}`}
+          onClick={() => {
+            setDisplayList("contact");
+          }}
+          active={displayList === "contact"}
+        >
+          Contacts
+        </Tab>
+      </div>
       {displayList === "conversation" ? (
         <div className={style.messageArea}>
           <ConversationList />
